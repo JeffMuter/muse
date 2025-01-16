7615b7616545d15517fce0d2959c2c8a09311bf9
JeffMuter <muterjeffery@gmail.com>
2025-01-16 18:27:59 +0000

    finished, and validated testing for the success of pelican, its now reading the incoming stream, and I can see the incoming packets...

--- go.mod
+++ go.mod
@@ -3,12 +3,17 @@ module muse
 go 1.23.3
 
 require (
-	github.com/aler9/gortsplib/v2 v2.2.2 // indirect
-	github.com/google/uuid v1.3.0 // indirect
+	github.com/aler9/gortsplib/v2 v2.2.2
+	github.com/bluenviron/gortsplib/v4 v4.12.1
+	github.com/pion/rtp v1.8.7-0.20240429002300-bc5124c9d0d0
+)
+
+require (
+	github.com/bluenviron/mediacommon v1.13.3 // indirect
+	github.com/google/uuid v1.6.0 // indirect
 	github.com/pion/randutil v0.1.0 // indirect
-	github.com/pion/rtcp v1.2.10 // indirect
-	github.com/pion/rtp v0.0.0-20230107162714-c3ea6851e25b // indirect
-	github.com/pion/sdp/v3 v3.0.6 // indirect
-	golang.org/x/net v0.8.0 // indirect
-	golang.org/x/sys v0.6.0 // indirect
+	github.com/pion/rtcp v1.2.15 // indirect
+	github.com/pion/sdp/v3 v3.0.9 // indirect
+	golang.org/x/net v0.33.0 // indirect
+	golang.org/x/sys v0.28.0 // indirect
 )
--- pelican/main.go
+++ pelican/main.go
@@ -2,10 +2,15 @@ package main
 
 import (
 	"context"
+	"log"
 	"os/exec"
 	"sync"
 
-	"github.com/aler9/gortsplib/v2"
+	"github.com/bluenviron/gortsplib/v4"
+	"github.com/bluenviron/gortsplib/v4/pkg/base"
+	"github.com/bluenviron/gortsplib/v4/pkg/description"
+	"github.com/bluenviron/gortsplib/v4/pkg/format"
+	"github.com/pion/rtp"
 )
 
 type StreamProcessor struct {
@@ -21,11 +26,43 @@ type StreamProcessor struct {
 func main() {
 	// rtsp listening
 	client := gortsplib.Client{}
-	err := client.Start(url, opts)
+
+	// parse url
+	url, err := base.ParseURL("rtsp://localhost:8554/stream")
+	if err != nil {
+		panic(err)
+	}
+
+	// connect server to stream
+	err = client.Start(url.Scheme, url.Host)
+	if err != nil {
+		panic(err)
+	}
+	defer client.Close()
+
+	desc, _, err := client.Describe(url)
+	if err != nil {
+		panic(err)
+	}
+
+	err = client.SetupAll(desc.BaseURL, desc.Medias)
+	if err != nil {
+		panic(err)
+	}
+
+	client.OnPacketRTPAny(func(media *description.Media, forma format.Format, pkt *rtp.Packet) {
+		log.Printf("RTP packet from media %v\n", media)
+	})
+
+	_, err = client.Play(nil)
+	if err != nil {
+		panic(err)
+	}
+
+	panic(client.Wait())
 
 	//cut out the audio with ffmpeg
 
 	// send audio to parrot microservice via grpc
 
-	return
 }
