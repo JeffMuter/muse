2c416ae7fc1dbd106a53e6569edd2981654f1a16
JeffMuter <muterjeffery@gmail.com>
2025-05-18 04:20:46 +0000

    figured out how to properly generate proto

--- parrot/main.go
+++ parrot/main.go
@@ -278,7 +278,7 @@ func sendEmailsFromMessages(messages map[string]string, sess *session.Session, c
 		}
 
 		// Create the gRPC message with Anthropic data
-		summary := &pb.TranscriptSummaryResponse{
+		summary := &pb.TranscriptSummaryRequest{
 			TranscriptionSummary: summaryData.TranscriptionSummary,
 			TranscriptId:         filepath.Base(fileName), // Using the new field we added
 			TranscriptionTopics:  make([]*pb.TranscriptionTopic, len(summaryData.TranscriptionTopics)),
